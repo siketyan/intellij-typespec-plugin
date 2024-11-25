e6c12571e5d0275e8ecbeed56b9c6cc2664c0c79
Naoki Ikeguchi <me@s6n.jp>
2024-11-26 01:18:48 +0900

    feat: Add original file icons for tsp

---
+++ src/main/kotlin/jp/s6n/idea/typespec/icons/TypeSpecIcons.kt
@@ -0,0 +1,8 @@
+package jp.s6n.idea.typespec.icons
+
+import com.intellij.openapi.util.IconLoader
+
+object TypeSpecIcons {
+    @JvmField
+    val Tsp = IconLoader.getIcon("/icons/tsp.svg", javaClass)
+}
--- src/main/kotlin/jp/s6n/idea/typespec/lang/TypeSpecFileType.kt
+++ src/main/kotlin/jp/s6n/idea/typespec/lang/TypeSpecFileType.kt
@@ -4,6 +4,7 @@ import com.intellij.icons.AllIcons
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiFile
+import jp.s6n.idea.typespec.icons.TypeSpecIcons
 import org.jetbrains.plugins.textmate.TextMateBackedFileType
 import org.jetbrains.plugins.textmate.TextMateFileType
 import javax.swing.Icon
@@ -12,7 +13,7 @@ object TypeSpecFileType : FileType, TextMateBackedFileType {
     override fun getName() = "TypeSpec"
     override fun getDescription() = "TypeSpec file"
     override fun getDefaultExtension() = "tsp"
-    override fun getIcon(): Icon = AllIcons.FileTypes.Json
+    override fun getIcon(): Icon = TypeSpecIcons.Tsp
     override fun isBinary() = false
 
     fun isMyFile(file: PsiFile) = isMyFile(file.virtualFile)
